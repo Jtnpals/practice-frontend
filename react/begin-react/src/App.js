5ccb4218fd0e9e902e77e315f7785b01f6d247dd
jtnpals <jtnpals@gmail.com>
2020-08-11 02:24:21 +0900

    Use jsx syntex

--- react/begin-react/src/App.js
+++ react/begin-react/src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import Hello from './Hello';
+import './App.css';
 
 function App() {
+  const name = 'react';
+  const style = {
+    backgroundColor: 'black',
+    color: 'aqua',
+    fontSize: 24,
+    padding: '1rem'
+  };
   return (
-    <div>
-      <Hello />
-      <Hello />
-      <Hello />
-    </div>
+    <>
+      {/* 주석 작성도 중괄호로 감쌈*/}
+      <Hello 
+        //태그내에는 이런식으로 주석 작성
+      />
+      <div style={style}>{name} 두개 이상의 태그는 감싸줘야함</div>
+      <div className="gray-box"></div>
+    </>
   );
 }
 
