b6fda5653adfe9c50d0ba5fc3a9c93fc1336d5f3
jtnpals <jtnpals@gmail.com>
2020-08-11 03:12:12 +0900

    Use props

--- react/begin-react/src/App.js
+++ react/begin-react/src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
 import Hello from './Hello';
-import './App.css';
+import Wrapper from './Wrapper';
 
 function App() {
-  const name = 'react';
-  const style = {
-    backgroundColor: 'black',
-    color: 'aqua',
-    fontSize: 24,
-    padding: '1rem'
-  };
   return (
-    <>
-      {/* 주석 작성도 중괄호로 감쌈*/}
-      <Hello 
-        //태그내에는 이런식으로 주석 작성
-      />
-      <div style={style}>{name} 두개 이상의 태그는 감싸줘야함</div>
-      <div className="gray-box"></div>
-    </>
+    <Wrapper>
+      <Hello name="react" color="red" />
+      <Hello color="pink" />
+    </Wrapper>
   );
 }
 
