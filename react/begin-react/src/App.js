c0085b80a039a5cb11a88a1efc80c4c9e842a574
jtnpals <jtnpals@gmail.com>
2020-08-19 00:33:51 +0900

    Use useRef

--- react/begin-react/src/App.js
+++ react/begin-react/src/App.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import UserList from './UserList'
 
 function App() {
+  const users = [
+    {
+        id: 1,
+        username: 'tester1',
+        email: 'tester1@gmail.com'
+    },
+    {
+        id: 2,
+        username: 'tester2',
+        email: 'tester2@gmail.com'
+    },
+    {
+        id: 3,
+        username: 'tester3',
+        email: 'tester3@gmail.com'
+    },
+  ]
+const nextId = useRef(4);
+const onCreate = () => {
+  console.log(nextId.current); //4
+  nextId.current += 1;
+}
+
   return (
-    <UserList />
+    <UserList users={users}/>
   );
 }
 
