846145f13ea03291f6b32f39e841e27b939b21d1
jtnpals <jtnpals@gmail.com>
2020-08-19 20:55:24 +0900

    Use useEffect

--- react/begin-react/src/UserList.js
+++ react/begin-react/src/UserList.js
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
 function User({ user, onRemove, onToggle }) {
+    useEffect(()=>{
+        console.log(user);
+        return () => {
+            console.log('user값이 바뀌기 전');
+            console.log(user);
+        }
+    }, [user]);
+    // useEffect(() =>{
+    //     console.log('컴포넌트가 화면에 나타남'); //props->state rest API  D3 Video.js setInterval etc  ...
+    //     return ()=>{
+    //         //clearInterval, clearTimeout, 라이브러리 인스턴스 제거
+    //         console.log('컴포넌트가 화면에서 사라짐');
+    //     }
+    // }, []);
   return (
     <div>
       <b style={{color: user.active ? 'blue' : 'black',
