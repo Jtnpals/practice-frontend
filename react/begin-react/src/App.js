02933a30e0c7b96788250e159bc495947aa2daa3
jtnpals <jtnpals@gmail.com>
2020-08-19 22:29:28 +0900

    Add custom hook

--- react/begin-react/src/App.js
+++ react/begin-react/src/App.js
@@ -1,76 +1,105 @@
-import React, {useRef, useState, useMemo, useCallback} from 'react';
+import React, {useRef, useReducer, useMemo, useCallback} from 'react';
 import UserList from './UserList'
 import CreateUser from './CreateUser';
+import useInputs from './useInputs';
 
 function countActiveUsers(users){
   console.log('활성 사용자 수를 세는 중');
   return users.filter(user => user.active).length;
 }
+
+const initialState ={
+  users: [
+    {
+      id: 1,
+      username: 'tester1',
+      email: 'tester1@gmail.com',
+      active: true
+  },
+  {
+      id: 2,
+      username: 'tester2',
+      email: 'tester2@gmail.com',
+      active: false
+  },
+  {
+      id: 3,
+      username: 'tester3',
+      email: 'tester3@gmail.com',
+      active: false
+  },
+  ]
+}
+
+function reducer(state, action){
+  switch (action.type){
+    case 'CREATE_USER':
+      return{
+        inputs: initialState.inputs,
+        users: state.users.concat(action.user)
+      }
+    case 'TOGGLE_USER':
+      return{
+        ...state,
+        users: state.users.map(user =>
+          user.id === action.id
+          ? {...user, active: !user.active}
+          :user
+        )
+      };
+    case 'REMOVE_USER':
+      return{
+        ...state,
+        users: state.users.filter(user => user.id !== action.id)
+      }
+      default:
+        throw new Error('Unhandled Action')
+  }
+}
+
 function App() {
-  const [inputs, setInputs] = useState({
-    username: '',
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const [form, onChange, reset] = useInputs({
+    username:'',
     email:'',
   });
-  const {username, email} = inputs;
-  const onChange = useCallback(e => {
-    const {name, value} = e.target;
-    setInputs({
-      ...inputs,
-      [name]:value
+  const {username, email} = form;
+  const nextId = useRef(4);
+  const { users } = state;
+
+  const onCreate = useCallback(()=>{
+    dispatch({
+      type: 'CREATE_USER',
+      user: {
+        id: nextId.current,
+        username,
+        email,
+      }
     });
-  }, [inputs]);
-  const [users, setUsers] = useState([
-    {
-        id: 1,
-        username: 'tester1',
-        email: 'tester1@gmail.com',
-        active: true
-    },
-    {
-        id: 2,
-        username: 'tester2',
-        email: 'tester2@gmail.com',
-        active: false
-    },
-    {
-        id: 3,
-        username: 'tester3',
-        email: 'tester3@gmail.com',
-        active: false
-    },
-  ]);
+    nextId.current += 1;
+    reset();
+  }, [username, email, reset])
 
-const onRemove = useCallback(id =>{
-  setUsers(users => users.filter(user => user.id !== id));
-}, []);
+  const onToggle = useCallback(id =>{
+    dispatch({
+      type: 'TOGGLE_USER',
+      id
+    });
+  }, []);
 
-const nextId = useRef(4);
-const onCreate = useCallback(() => {
-  const user = {
-    id: nextId.current,
-    ...inputs
-  };
-  setUsers(users => [...users, user]); // setUsers(users.concat(user)); push, splice 등 사용하면안됨 불변성유지
-  setInputs({
-    username: '',
-    email: ''
-  })
-  console.log(nextId.current); //4
-  nextId.current += 1;
-}, [username, email]);
+  const onRemove = useCallback(id =>{
+    dispatch({
+      type: 'REMOVE_USER',
+      id
+    });
+  }, [])
 
-const onToggle = useCallback(id =>{
-  setUsers(users => users.map(
-    user => user.id === id ? {...user, active: !user.active}:user
-  ));
-}, []);
-
-const count = useMemo(()=>countActiveUsers(users), [users]);
+  const count = useMemo(()=> countActiveUsers(users), [users])
 
   return (
     <>
     <CreateUser username={username} email={email} onChange={onChange} onCreate={onCreate}/>
-    <UserList users={users} onRemove={onRemove} onToggle={onToggle}/>
+    <UserList users={users} onToggle={onToggle} onRemove={onRemove}/>
     <div>활성 사용자 수: {count}</div>
     </>
   );
