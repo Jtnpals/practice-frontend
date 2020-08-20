a6a8b9b5ea4995096d67dc1a3fd3fcfb73c42772
jtnpals <jtnpals@gmail.com>
2020-08-21 06:53:16 +0900

    Add TodoList sample app

---
+++ react/mashup-todolist/src/components/TodoList.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import styled from 'styled-components';
+import TodoItem from './TodoItem';
+import { useTodoState } from '../TodoContext';
+
+const TodoListBlcok = styled.div`
+  flex: 1;
+  padding: 20px 32px;
+  padding-bottom: 48px;
+  overflow-y: auto;
+`;
+
+function TodoList() {
+  const todos = useTodoState();
+  return (
+    <TodoListBlcok>
+      {todos.map((todo) => (
+        <TodoItem key={todo.id} id={todo.id} text={todo.text} done={todo.done} />
+      ))}
+    </TodoListBlcok>
+  );
+}
+
+export default TodoList;
