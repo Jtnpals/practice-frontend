a44da7def3c043421ba7dee8849f00c5c45ae7d5
sumin jin <jtnpals@gmail.com>
2021-07-19 19:48:24 +0900

    Test typescript

---
+++ typescript/src/util.ts
@@ -0,0 +1,8 @@
+//주어진 두수의 합을 구하는 함수
+export default function add(a,b){
+ return a+b    
+}
+
+export function minus(a,b){
+    return a-b
+}
---
+++ typescript/src/variables.ts
@@ -0,0 +1,11 @@
+var score1 =0;
+let score2 =0;
+const defaultScore =0;
+
+function outer(){
+    var score = 0;
+    function inner(){
+        console.log(score);
+    }
+
+}
